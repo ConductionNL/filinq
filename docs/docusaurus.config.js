a7612005f80adaff2e5730eebbea9e85e6f03fce
Ruben van der Linde <rubenvdlinde@gmail.com>
2026-03-19 20:02:13 +0100

    fix: exclude node_modules from Docusaurus docs path

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -30,6 +30,7 @@ const config = {
       ({
         docs: {
           path: './',
+          exclude: ['**/node_modules/**'],
           sidebarPath: require.resolve('./sidebars.js'),
           editUrl:
             'https://github.com/conductionnl/docudesk/tree/main/docs/',
