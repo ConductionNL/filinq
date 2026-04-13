49ba22803c9a7a9fb8747da176daa2910f804ae5
Robert Zondervan <robert@conduction.nl>
2026-04-13 13:45:32 +0200

    style: align equals sign in FolderBatchService to satisfy phpcs

--- lib/Service/FolderBatchService.php
+++ lib/Service/FolderBatchService.php
@@ -111,7 +111,7 @@ class FolderBatchService
         // returned regardless of which input method the caller used.
         $resolvedFolderId   = $node->getId();
         $resolvedFolderPath = $userFolder->getRelativePath($node->getPath()) ?? $node->getPath();
-        $inputMethod = 'path';
+        $inputMethod        = 'path';
         if ($folderId !== null) {
             $inputMethod = 'id';
         }
